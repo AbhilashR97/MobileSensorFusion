3019f8de2d76a8daeffeb7b54910645acc889955
AbhilashR97 <57861037+AbhilashR97@users.noreply.github.com>
2020-05-24 15:17:12 +0200

    Upto Task 10

--- Sensorfusion smartphone/Helper functions/Myfilter.m
+++ Sensorfusion smartphone/Helper functions/Myfilter.m
@@ -27,7 +27,28 @@ function [xhat, meas] = filterTemplate(calAcc, calGyr, calMag)
   t0 = [];  % Initial time (initialize on first data received)
   nx = 4;   % Assuming that you use q as state variable.
   % Add your filter settings here.
-  Rw = eye(3);  
+  Rw = [6.49387879914775e-07,-1.42534248411078e-08,4.97196329600802e-08;
+        -1.42534248411078e-08,1.01729591677740e-06,2.63720827134863e-08;
+        4.97196329600802e-08,2.63720827134863e-08,6.71290025517459e-07];
+    
+  Ra = [0.000136329334910749,-1.77530195827294e-07,3.74440800946149e-07;
+        -1.77530195827294e-07,0.000144713181506337,-1.26164948448555e-05;
+        3.74440800946149e-07,-1.26164948448555e-05,0.000202076528637777];
+    
+  g0 = [0.115501569664997;-0.296649960504062;9.86741791140054];
+  
+  accOut = 0;
+  magOut = 0;
+  
+  m0 = [-14.5161348443092;-6.57631458403429;-93.6254289214554];
+  
+  Rm = [0.118502459469225,-0.0109745354499427,-0.0972133850892457;
+        -0.0109745354499427,0.114499214536508,-0.0630803937921023;
+        -0.0972133850892457,-0.0630803937921023,1.81876242147819];
+    
+  gyr_bool = false;
+  acc_bool = false;
+  mag_bool = false;
   
   % Current filter state.
   x = [1; 0; 0 ;0];
@@ -78,23 +99,52 @@ function [xhat, meas] = filterTemplate(calAcc, calGyr, calMag)
       acc = data(1, 2:4)';
       if ~any(isnan(acc))  % Acc measurements are available.
         % Do something
+        acc_bool = true;
       end
       gyr = data(1, 5:7)';
       if ~any(isnan(gyr))  % Gyro measurements are available.
         % Do something
-          dt = t - xhat.t(end) - t0;
-          [x, P] = tu_qw(x, P, gyr, dt, Rw);       % Time update of EKF
-          [x, P] = mu_normalizeQ(x, P);
+        gyr_bool = true;
       end
 
       mag = data(1, 8:10)';
       if ~any(isnan(mag))  % Mag measurements are available.
         % Do something
+        mag_bool = true;
       end
       
-%       dt = t - xhat.t(end) - t0;
-%       [x, P] = tu_qw(x, P, gyr, dt, Rw);       % Time update of EKF
-%       [x, P] = mu_normalizeQ(x, P);
+      if gyr_bool == true
+          dt = t - t0 - xhat.t(end);
+          
+          [x, P] = tu_qw(x, P, gyr, dt, Rw);       % Time update of EKF
+          [x, P] = mu_normalizeQ(x, P);
+          gyr_bool = false;
+      end
+      
+      if acc_bool == true 
+          if abs(9.81 - norm(acc)) <= 1e-1
+              [x, P] = mu_acc(x, P, acc, Ra, g0);  
+              [x, P] = mu_normalizeQ(x, P);
+              accOut = 0;
+          else
+              accOut = 1;
+          end
+          ownView.setAccDist(accOut)
+          acc_bool = false;
+      end
+      
+      if mag_bool == true
+          L = 
+          if abs(L - norm(mag)) < = 1e-1
+            [x, P] = mu_m(x, P, mag, m0,Rm);
+            [x, P] = mu_normalizeQ(x, P);
+            magOut = 0;
+          else
+              magOut = 1;
+          end
+          ownView.setMagDist(magOut);
+            mag_bool = false;
+      end
       
       orientation = data(1, 18:21)';  % Google's orientation estimate.
 
